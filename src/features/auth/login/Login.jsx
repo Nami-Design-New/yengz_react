46aa1ae5f9fd7697b2ba2e7a443fbdddd51b30e5
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-06-27 13:03:10 +0300

    form

--- src/features/auth/login/Login.jsx
+++ src/features/auth/login/Login.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Google from "../../../Assets/images/Google.svg";
+import InpuFeild from "../../../ui/form-elements/InputField";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
 
   return (
     <main>
@@ -13,14 +25,13 @@ const Login = () => {
         </p>
         <form action="register" className="container form">
           <div className="input-field col-12">
-            <label htmlFor="email">
-              <i className="ti ti-md ti-mail"></i> البريد الالكتروني
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="مثال : mail@mail.com"
+            <InpuFeild
+              label={"البريد الالكتروني"}
+              name={"email"}
+              id={"email"}
+              placeholder={"example@example.com"}
+              value={formData.email}
+              onChange={(e) => handleChange(e)}
             />
           </div>
           <div className="input-field col-12">
