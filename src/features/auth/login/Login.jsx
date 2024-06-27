e2a76a751898b2ba85c175955b6e5b78faddd126
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-06-27 14:49:47 +0300

    #login first

--- src/features/auth/login/Login.jsx
+++ src/features/auth/login/Login.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Google from "../../../Assets/images/Google.svg";
 import InpuFeild from "../../../ui/form-elements/InputField";
+import PasswordField from "./../../../ui/form-elements/PasswordField";
+import axios from "./../../../utils/axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
-    password: ""
+    password: "",
+    token: "1"
   });
 
   const handleChange = (e) => {
@@ -16,6 +22,27 @@ const Login = () => {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = axios.post("/user/login", formData);
+      if (res.status === 200) {
+        toast.success("تم تسجيل الدخول بنجاح");
+        navigate("/");
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${res.data.data.token}`;
+      } else {
+        toast.error("البريد الالكتروني او كلمة المرور غير صحيحة");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <main>
       <section className="login-section container">
@@ -23,29 +50,29 @@ const Login = () => {
         <p className="text-center mt-3 title">
           اهلا بك ...! ادخل بياناتك المسجلة لإتمام عملية الدخول
         </p>
-        <form action="register" className="container form">
-          <div className="input-field col-12">
+        <form className="container form" onSubmit={handleSubmit}>
+          <div className="col-12 p-2">
             <InpuFeild
               label={"البريد الالكتروني"}
               name={"email"}
               id={"email"}
+              type={"email"}
+              required={true}
               placeholder={"example@example.com"}
               value={formData.email}
               onChange={(e) => handleChange(e)}
             />
           </div>
-          <div className="input-field col-12">
-            <label htmlFor="password">
-              <i className="ti ti-md ti-lock"></i> كلمة المرور
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="***********"
+          <div className="col-12 p-2">
+            <PasswordField
+              label={"كلمة المرور"}
+              name={"password"}
+              id={"password"}
+              value={formData.password}
+              onChange={(e) => handleChange(e)}
             />
           </div>
-          <Link to="/forget_password" className="forgetpass">
+          <Link to="/forget-password" className="forgetpass">
             نسيت كلمة المرور ؟
           </Link>
           <button type="submit">تسجيل الدخول</button>
