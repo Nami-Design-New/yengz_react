8e2774c24037a275c99d1c53209f2f4fb0a4b2d2
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-07-10 15:09:55 +0300

    increase and handle cart

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -4,10 +4,39 @@ import { useTranslation } from "react-i18next";
 import CartBox from "../ui/cart/CartBox";
 import useCartList from "../features/cart/useCartList";
 import emptyCart from "../Assets/images/emptyCart.svg";
+import { useDispatch } from "react-redux";
+import { updateEntireCart } from "../redux/slices/cart";
+import { deleteCart } from "../services/apiCart";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 const Cart = () => {
   const { data: cart } = useCartList();
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  dispatch(
+    updateEntireCart(
+      cart?.data?.map((item) => ({
+        id: item.id,
+        service_id: item.service?.id,
+        quantity: item.quantity,
+        developments: item?.service?.developments?.map(
+          (dev) => dev.in_cart && dev.id
+        )
+      }))
+    )
+  );
+
+  const queryClient = useQueryClient();
+  const handleDelete = async () => {
+    try {
+      await deleteCart(queryClient);
+      toast.success(t("cart.cartDelted"));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <main>
@@ -32,6 +61,11 @@ const Cart = () => {
                     <Link className="order-now" to="/checkout">
                       {t("services.orderNow")}
                     </Link>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <button onClick={handleDelete} className="order-now delete">
+                      {t("cart.deleteCart")}
+                    </button>
                   </div>
                 </div>
               </div>
