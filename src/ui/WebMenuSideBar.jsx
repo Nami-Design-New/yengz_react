0ff4e8588c062854ec2bb045500ecf36745f68d1
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-08-19 11:34:19 +0300

    navbar updates

--- src/ui/WebMenuSideBar.jsx
+++ src/ui/WebMenuSideBar.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { IconExternalLink } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import useGetAbout from "../features/About/useGetAbout";
+import { Accordion } from "react-bootstrap";
+import logo from "../Assets/images/logo.svg";
 
 function WebMenuSideBar({ isOpen, setIsOpen }) {
   const { t } = useTranslation();
@@ -23,19 +24,57 @@ function WebMenuSideBar({ isOpen, setIsOpen }) {
       <ul className="nav_side_menu">
         <li>
           <Link to="/portfolios" onClick={() => setIsOpen(false)}>
-            <IconExternalLink stroke={2} /> {t("navbar.portfolios")}
+            <i className="fa-regular fa-file-invoice"></i>{" "}
+            {t("navbar.portfolios")}
           </Link>
         </li>
-        {footerCategoriesList?.map((category) => (
-          <li key={category.id}>
-            <Link to={`/about/${category.id}`}>
-              <IconExternalLink stroke={2} /> {category.name}
-            </Link>
-          </li>
-        ))}
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <img src={logo} alt="" />
+              <span>{t("navbar.ynjez")}</span>
+            </Accordion.Header>
+            <Accordion.Body>
+              {footerCategoriesList?.map((category) => (
+                <li key={category.id}>
+                  <Link
+                    to={`/about/${category.id}`}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <i class="fa-sharp fa-solid fa-circle-info"></i>{" "}
+                    {category.name}
+                  </Link>
+                </li>
+              ))}
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>
+              <i class="fa-solid fa-comments"></i>{" "}
+              <span>{t("navbar.communities")}</span>
+            </Accordion.Header>
+            <Accordion.Body>
+              <li>
+                <Link to="" onClick={() => setIsOpen(false)}>
+                  نماذج أعمال قمت بتنفيذها
+                </Link>
+              </li>
+              <li>
+                <Link to="" onClick={() => setIsOpen(false)}>
+                  طلبات الخدمات غير الموجودة
+                </Link>
+              </li>
+              <li>
+                <Link to="" onClick={() => setIsOpen(false)}>
+                  تجارب وقصص المستخدمين
+                </Link>
+              </li>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
         <li>
-          <Link to="/blogs">
-            <IconExternalLink stroke={2} /> {t("footer.blogs")}
+          <Link to="/blogs" onClick={() => setIsOpen(false)}>
+            <i class="fa-solid fa-blog"></i> {t("navbar.blogs")}
           </Link>
         </li>
       </ul>
