03f9ac13e59766d7df3a4978bdc3380d406711b5
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-08-19 09:43:51 +0300

    portfolios page

--- src/ui/cards/PortfolioCard.jsx
+++ src/ui/cards/PortfolioCard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import image from "../../Assets/images/Artboard.png";
+import { Link } from "react-router-dom";
 
 function PortfolioCard() {
   return (
-    <div className="portfolio-card">
+    <Link to="/portfolios" className="portfolio-card">
       <div className="img">
         <img src={image} alt="" />
       </div>
-    </div>
+      <div className="info">
+        <h6>من اخر اعمالي في تصميم الشعارات</h6>
+        <ul>
+          <li>
+            <i class="fa-sharp fa-regular fa-eye"></i> 3
+          </li>
+          <li>
+            <i class="fa-sharp fa-solid fa-heart"></i> 4
+          </li>
+        </ul>
+      </div>
+    </Link>
   );
 }
 
